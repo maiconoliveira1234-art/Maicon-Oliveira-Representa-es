76dd546922b3420dd129f9a92d7183ea90b2cb5e
maiconoliveira1234-art <maicon.oliveira1234@gmail.com>
2026-04-23 10:29:36 -0300

    fix: Update app version to 1.19.3

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.19.2';
+export const APP_VERSION = '1.19.3';
 export const SALES_CUTOFF_DATE = '2026-04-11';
 export const SALES_CUTOFF_CLIENTS = [
   'LUCIA IRIA SCHNEIDER FLORES',
